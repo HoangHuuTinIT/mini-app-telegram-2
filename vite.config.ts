bdf8f814959e5e88d86ed45b7719a4b08b8c3e22
HoangHuuTinIT <tin03bk@gmail.com>
2025-12-30 19:59:39 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vuejs-template',
+  base: './',
   plugins: [
     vue(),
     vueDevTools(),
@@ -25,5 +25,8 @@ export default defineConfig({
   server: {
     // Exposes your dev server and makes it accessible for the devices in the same network.
     host: true,
+  },
+  build: {
+    target: 'esnext',
   },
 })
